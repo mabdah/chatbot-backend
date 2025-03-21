6b0df27e68d538b1e53adf3e07385b521637b0ba
mabdah <mabisha@telerivet.com>
2025-03-21 15:54:16 +0545

    new changes added

--- app.js
+++ app.js
@@ -42,8 +42,10 @@ app.post('/sendMessage', (req, res) => {
     console.log(message, "object78");
 
     try {
-
-        socket.emit('user-message', message); // Emit message to WebSocket server
+        socket.on("connection", () => {
+            socket.emit('user-message', message);
+        })
+        // Emit message to WebSocket server
         res.json({ success: true });
 
     } catch (error) {
