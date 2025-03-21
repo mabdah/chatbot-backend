16cc0f031aea8c8bb3a17d2f3d5b6d0713197966
mabdah <mabisha@telerivet.com>
2025-03-21 16:39:42 +0545

    new changes

--- app.js
+++ app.js
@@ -2,31 +2,31 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
+
 const app = express();
 app.use(express.json());
-
-// CORS Configuration
-const corsOptions = {
-    origin: '*',  // Allow requests from any origin
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type'],
-};
-app.use(cors(corsOptions));
+app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
-app.post('/', (req, res) => {
-    console.log("hello world");
 
+app.get('/', (req, res) => {
+    res.send('Hello World');
 });
+
 app.post('/sendMessage', (req, res) => {
     const { message } = req.body;
-    console.log(message, "received in backend");
+    console.log(message, "object78");
 
-    res.json({ success: true });
+    try {
+        res.json({ success: true, value: message });
+
+    } catch (error) {
+        console.error(" error:", error);
+        res.status(500).json({ error: "connection failed" });
+    }
 });
 
-// Start Server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
