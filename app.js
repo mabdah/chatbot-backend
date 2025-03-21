b1fb13514abed5abbd50e9c9bbc4502bee653530
mabdah <mabisha@telerivet.com>
2025-03-21 12:18:18 +0545

    packages added

--- app.js
+++ app.js
@@ -1,32 +1,37 @@
+// server.js
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const axios = require("axios");
+const socketIoClient = require("socket.io-client");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-const PORT = process.env.PORT || 3000
-const API_BASE_URL = process.env.API_BASE_URL;
-const PROJECT_ID = process.env.PROJECT_ID;
-const API_KEY = process.env.API_KEY;
-const SEND_MESSAGE_URL = `${API_BASE_URL}/projects/${PROJECT_ID}/messages/send`
+const PORT = process.env.PORT || 3000;
+const SOCKET_SERVER_URL = 'http://localhost:3001'; // WebSocket server URL
+
+const socket = socketIoClient(SOCKET_SERVER_URL);
+
 app.get('/', (req, res) => {
-    res.send('Hello World')
-})
+    res.send('Hello World');
+});
+
 app.post('/sendMessage', (req, res) => {
-    const { message, } = req.body;
-    console.log(message, "object78")
+    const { message } = req.body;
+    console.log(message, "object78");
+
+    // Emit the received message to all WebSocket clients
+    socket.emit('message', message); // Emit message to WebSocket server
+
     try {
-        res.json({ success: true, })
+        res.json({ success: true });
     } catch (error) {
         console.error("Error:", error);
         return res.status(500).json({ error: JSON.stringify(error) });
     }
 });
 
-
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-})
+});
