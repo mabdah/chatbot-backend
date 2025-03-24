6f862f3ec03d8ee02c7a9dc2c3a64d333dd26543
mabdah <mabisha@telerivet.com>
2025-03-24 22:36:44 +0545

    new api changes

--- app.js
+++ app.js
@@ -1,19 +1,44 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 const PORT = process.env.PORT || 3000;
-
+const TELERIVET_INCOMING_URL = "https://api.telerivet.com/gateway/PNddded6f1601b45f8/446f0f6b8b/incoming";
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
 let storedMessage = ""; // Global variable to store the message
+app.post("/send", async (req, res) => {
+    const { textMessage, number } = req.body;
+    console.log(number, "this is message")
+    if (!number) {
+        var uniqueTestNumber = "555" + Math.floor(1000 + Math.random() * 9000);
+    } else {
+        var uniqueTestNumber = number
+    }
+    var name = "ChatBot test"
+    console.log(`Received uniqueTestNumber: ${uniqueTestNumber}`);
+    try {
+        // Forward message to Telerivet
+        const response = await axios.post(TELERIVET_INCOMING_URL, {
+            name: name,
+            from_number: uniqueTestNumber,
+            content: textMessage,
+        });
+        const responseData = JSON.parse(response.config.data)
+        return res.json({ success: true, value: { uniqueTestNumber: responseData.from_number, message: responseData.content } });
 
+    } catch (error) {
+        console.error("Error forwarding message:", error.message);
+        res.status(500).json({ success: false, error: error.message });
+    }
+});
 app.post("/sendMessage", (req, res) => {
     const { message } = req.body;
     console.log(message, "message sent to vercel api")
